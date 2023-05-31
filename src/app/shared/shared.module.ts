3b77fdedb63ab77864c6610151ddcd70646029e1
icheld123 <icheldmorales@gmail.com>
2023-05-31 11:42:04 -0500

    Adicion fotos de monitores y lider

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,7 +2,7 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { ReactiveFormsModule } from "@angular/forms";
-import {NgxPaginationModule} from 'ngx-pagination';
+import { NgxPaginationModule } from 'ngx-pagination';
 
 @NgModule({
   declarations: [
